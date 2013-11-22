e2331c66b1bf01002fbae085ea49dddce177fd99
wangzhengyi <15866883396@126.com>
2013-11-22 11:06:12 +0800

    ac了求二叉树深度的题目

---
+++ src/MaximumDepthOfBtree.java
@@ -0,0 +1,29 @@
+public class MaximumDepthOfBtree {
+	public static void main(String args[]) {
+	}
+
+	public static int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		} else if (root.left == null && root.right == null) {
+			return 1;
+		} else {
+			int ldepth, rdepth;
+
+			ldepth = maxDepth(root.left);
+			rdepth = maxDepth(root.right);
+
+			return ldepth > rdepth ? ldepth + 1 : rdepth + 1;
+		}
+	}
+}
+
+class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int x) {
+		this.val = x;
+	}
+}

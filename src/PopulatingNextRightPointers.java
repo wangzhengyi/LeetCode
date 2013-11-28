edb340958a16195b88120533c9ae5e13685c17b5
wangzhengyi <15866883396@126.com>
2013-11-28 18:12:57 +0800

    ac了一道简单的题目，今天下午效率有点太低了

---
+++ src/PopulatingNextRightPointers.java
@@ -0,0 +1,25 @@
+public class PopulatingNextRightPointers {
+    static class TreeLinkNode {
+        int val;
+        public TreeLinkNode left, right, next;
+
+        public TreeLinkNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public static void connect(TreeLinkNode root) {
+        if (root == null) return;
+
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+
+        if (root.right != null) {
+            root.right.next = root.next == null ? null : root.next.left;
+        }
+
+        connect(root.left);
+        connect(root.right);
+    }
+}

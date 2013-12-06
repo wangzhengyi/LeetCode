a37e6b74345150675be98a0b0bf9ecee43af7f63
wangzhengyi <wzyll1314@gmail.com>
2013-12-06 10:50:00 +0800

    哈哈，用顺序遍历ac了这道题目

---
+++ src/SearchInRotatedArrayII.java
@@ -0,0 +1,12 @@
+
+public class SearchInRotatedArrayII {
+    public boolean search(int[] A, int target) {
+        for (int i = 0; i < A.length; i ++) {
+            if (A[i] == target) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}

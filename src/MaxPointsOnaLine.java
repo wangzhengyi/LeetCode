f338889d46f747dd6d40755a0afd6169bb12424e
wangzhengyi <wzyll1314@gmail.com>
2014-06-03 16:50:14 +0800

    两次for循环解决了所有点构成直线中点最多的直线问题

---
+++ src/MaxPointsOnaLine.java
@@ -0,0 +1,61 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxPointsOnaLine {
+    static class Point {
+        int x;
+        int y;
+
+        Point() {
+            x = 0;
+            y = 0;
+        }
+
+        Point(int a, int b) {
+            this.x = a;
+            this.y = b;
+        }
+    }
+
+    public static int maxPoints(Point[] points) {
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+        int max = 0;
+
+        HashMap<Double, Integer> collectMap = new HashMap<Double, Integer>();
+        
+        for (int i = 0; i < points.length; i++) {
+            collectMap.clear();
+            int duplicate = 1;
+            for (int j = 0; j < points.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                
+                if (points[i].x == points[j].x && points[i].y == points[j].y) {
+                    duplicate ++;
+                    continue;
+                }
+                double k = points[i].x == points[j].x ? Integer.MAX_VALUE : (1.0 * (points[j].y - points[i].y)) / (points[j].x - points[i].x);
+                if (collectMap.get(k) != null) {
+                    collectMap.put(k, collectMap.get(k) + 1);
+                } else {
+                    collectMap.put(k, 1);
+                }
+            }
+            
+            if (duplicate > max) {
+                max = duplicate;
+            }
+            
+            for (Integer num : collectMap.values()) {
+                if (num + duplicate > max) {
+                    max = num + duplicate;
+                }
+            }
+        }
+
+        return max;
+    }
+}

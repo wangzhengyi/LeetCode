b7d4dc534410e49e6ee37021a4e9b8b0f7de172e
wangzhengyi <wzyll1314@gmail.com>
2015-02-24 15:43:14 +0800

    ac质因子题目

---
+++ src/FactorialTrailingZeroes.java
@@ -0,0 +1,14 @@
+
+public class FactorialTrailingZeroes {
+	
+	public int trailingZeroes(int n) {
+		int res = 0;
+		
+		while (n >= 5) {
+			res += n / 5;
+			n /= 5;
+		}
+		
+		return res;
+	}
+}

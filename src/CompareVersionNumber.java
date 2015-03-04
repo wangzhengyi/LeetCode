86915cd748809ea53028ac9b08c215b553c99c0d
wangzhengyi <wzyll1314@gmail.com>
2015-03-04 15:47:27 +0800

    optimize compare version number code

--- src/CompareVersionNumber.java
+++ src/CompareVersionNumber.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CompareVersionNumber {
@@ -7,47 +6,34 @@ public class CompareVersionNumber {
 			return 0;
 		}
 		
-		ArrayList<Integer> list1 = splitStr(version1);
-		ArrayList<Integer> list2 = splitStr(version2);
+		String[] version1Array = version1.split("\\.");
+		String[] version2Array = version2.split("\\.");
 		
-		int len = list1.size() < list2.size() ? list1.size() : list2.size();
+		int index = 0;
+		int minLen = Math.min(version1Array.length, version2Array.length);
+		int diff = 0;
 		
-		int i;
-		for (i = 0; i < len; i ++) {
-			if (list1.get(i) > list2.get(i)) {
-				return 1;
+		while (index < minLen && (diff = Integer.parseInt(version1Array[index]) - Integer.parseInt(version2Array[index])) == 0) {
+			index ++;
+		}
+		
+		if (diff == 0) {
+			for (int i = index; i < version1Array.length; i ++) {
+				if (Integer.parseInt(version1Array[i]) > 0) {
+					return 1;
+				}
 			}
 			
-			if (list1.get(i) < list2.get(i)) {
-				return -1;
+			for (int i = index; i < version2Array.length; i ++) {
+				if (Integer.parseInt(version2Array[i]) > 0) {
+					return -1;
+				}
 			}
+			
+			return 0;
+		} else {
+			return diff > 0 ? 1 : -1;
 		}
-		
-		int j;
-		
-		for (j = i; j < list1.size(); j ++) {
-			if (list1.get(j) > 0) {
-				return 1;
-			}
-		}
-		
-		for (j = i; j < list2.size(); j ++) {
-			if (list2.get(j) > 0) {
-				return -1;
-			}
-		}
-		
-		return 0;
-	}
-
-	
-	private static ArrayList<Integer> splitStr(String str) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (String s : str.split("\\.")) {
-			list.add(Integer.parseInt(s));
-		}
-		
-		return list;
 	}
 	
 	public static void main(String[] args) {

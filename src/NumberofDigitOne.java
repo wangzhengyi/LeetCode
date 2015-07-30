af17139cbafac80fb15d45155959cc343593b164
wangzhengyi <wzyll1314@gmail.com>
2015-07-30 12:38:11 +0800

    解决1个数问题

---
+++ src/NumberofDigitOne.java
@@ -0,0 +1,36 @@
+public class NumberofDigitOne {
+	public int countDigitOne(int n) {
+		int count = 0;
+		long factor = 1;
+		long cdigit = 0;
+		long highN = 0;
+		int lowN = 0;
+		
+		while (n / factor > 0) {
+			cdigit = (n % (factor * 10)) / factor;
+			highN = n / (factor * 10);
+			
+			if (cdigit == 0) {
+				count += highN * factor;
+			} else if (cdigit == 1) {
+				count += highN * factor + lowN + 1;
+			} else {
+				count += (highN + 1) * factor;
+			}
+			
+			lowN += cdigit * factor;
+			factor *= 10;
+		}
+		
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		int n = 1410065408;
+		NumberofDigitOne ndo = new NumberofDigitOne();
+		int count = ndo.countDigitOne(n);
+		
+		System.out.println(count);
+	}
+}
